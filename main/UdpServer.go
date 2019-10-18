428fee86cc9c1412c9dc6b40685bc6dbdf1e5d00
chenkaiju <1750994840@qq.com>
2019-10-18 21:11:45 +0800

    增加updserver
    Change-Id: I253369c699334c4012238fcf4926a5a135d4faec

---
+++ main/UdpServer.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+var host = flag.String("host", "", "host")
+var port = flag.String("port", "65082", "port")
+
+func main() {
+	flag.Parse()
+	addr, err := net.ResolveUDPAddr("udp", *host+":"+*port)
+	if err != nil {
+		fmt.Println("Can't resolve address: ", err)
+		os.Exit(1)
+	}
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+	for {
+		handleClient(conn)
+	}
+}
+func handleClient(conn *net.UDPConn) {
+	data := make([]byte, 65500)
+	n, remoteAddr, err := conn.ReadFromUDP(data)
+	if err != nil {
+		fmt.Println("failed to read UDP msg because of ", err.Error())
+		return
+	}
+	fmt.Println("Recived data:\n", string(data))
+	daytime := time.Now().Unix()
+	fmt.Println(n, remoteAddr)
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, uint32(daytime))
+	conn.WriteToUDP(b, remoteAddr)
+}
